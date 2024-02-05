79951846a4c0ab03e6ce3e97d0091cf61d05cfe5
Noé Edouard <noedouard31@gmail.com>
2024-02-05 08:15:33 +0100

    Deuxième session de travail. Fin des bases, début du work flow

--- 01. Basics/app.js
+++ 01. Basics/app.js
@@ -1,5 +1,36 @@
-console.log("Hello world");
-alert("carefull");
-console.warn("be careful");
+// I VARIABLES
+/*
+varible = reserved keyword + name + type of value
 
-console.clear();
+keyword : var (never used) / let (editable) / const (not editable)
+
+Le nom d'une variable ne peut pas être un mot clé, ne peut pas commencer par un chiffre et ne peut pas contenir -. Les noms de variable sont par convention écrit en chamel case.
+*/
+
+let name = "Nono";
+let whatDoYouWantToBecomeWithYourLife = "I have no clue at all";
+let gender = "male";
+
+console.log(
+  "My name is",
+  name,
+  whatDoYouWantToBecomeWithYourLife,
+  "I am a ",
+  gender
+);
+
+// II PRIMITIVE TYPES
+// numbers
+// booleans (flasy values (false, null, undefined, 0, -0, NaN, '', "", ``) / true (else) / Nam= Not a Number)
+// Comparison operators (=== : "type + value", !== : "type + value", == "value", != : "value")
+// Strings ("", '', ``) / methodes : firstName.concat(secondName) ajoute un espace entre les strings, string.length, string.toLower/UpperCase(), string.slice(start, end-excluded), string.split(splitter: '') : return an array, array.join(separator : ' '), string.includes('') : return boolean, string.trim() : surpprime les espaces au début et à la fin
+let firstName = "Coco";
+let secondName = "channel";
+let fullName = `My full name is ${firstName} ${secondName}`;
+console.log(fullName);
+
+// Convert string to number : num = parseInt(str) ou num = Number(money) ou num = parseFloat(str)
+// Convert number to string : str = num.toString() ou  str = String(num)
+
+// La différence entre null et undefined est que null peut être imposer à une variable par le programmer alors que undefined l'est par la console
+// Pour afficher le type d'une variable, on utilise typeof(variable)
---
+++ 02. Control Flow/app2.js
@@ -0,0 +1,238 @@
+/* CONDITION STATEMENTS
+Evalue des conditions distinctes
+if (condition) {
+    function to execute
+} else if {
+    function to execute
+} else {
+    last function
+}
+*/
+
+/* SWITCH STATEMENT 
+Evalue une condition une seule fois et la compare au différents cas
+
+let jour = "Lundi";
+
+switch (jour) {
+    case "Lundi":
+        console.log("C'est lundi !");
+        break;
+    case "Mardi":
+        console.log("C'est mardi !");
+        break;
+    default:
+        console.log("C'est un autre jour de la semaine.");
+}
+*/
+
+/* LOOPS
+for (let index, end_condition on index, index++) {
+    function
+}
+
+while (condition) {
+    function
+    decrement
+}
+*/
+
+/* LOGICAL OPERATORS
+and (&&)
+or (||)
+not (!)
+*/
+
+/* ARRAYS METHODES
+concat : join two or more arrays into one
+include : check if an array contains a specific element
+push(elements, array) : adds a new element at the end of the array and returns the new length
+unshift : adds a new element at the beggining of the array and returns the new length
+shift : removes the first item of an array and returns the array
+pop() : removes the last item of the array and returns that item
+sort() : sorts the element according to the methode passed as argument, sorts by default alphabetically for strings and acendinglingly for arrays
+slice() : selects parts of an array and returns the new array
+splice() : removes or replace existing elements and/or adds new elements
+join("-") : returns a string made of the elements of the array separated by "-"
+k
+*/
+
+/* OBJECTS
+object = {key : value}
+we can access, modifie or add a specific values with obj.key or obj['key']
+to delete a data : delete obj.key
+*/
+
+/* FUNCTIONS
+reusable block of code
+
+function name(parameters) {
+    return statement
+}
+call de la function : name(argument)
+
+Fonction declaration : function name(para) {}
+
+Fonction expression : const name(para) {}
+On ne appeler une expression de fonction que après l'avoir écrite
+
+Callback function when the argument is a function : function name(fn) {
+    const value = 10;
+    fn(value)
+}
+*/
+
+/* SCOPES
+Global scopes variable : can be accessed from everywhere in the code
+Local variable : can only be access in a block {}
+*/
+
+/* METHODES
+const name = {
+    key: value;
+    fn: function () {
+        return ...
+    }
+
+A methode is a function associated to an object
+}
+
+*/
+
+/* JSON
+Used to transmit  between severs or storing data 
+{
+    "name": value (attention "" obligatoire et pas '')
+}
+Dans js :
+const person = {
+  "name": "John",
+  "age": 30,
+  "hobbies": ["readign", "running"],
+  "address": {
+    "city": "NY",
+    "idk": "true"
+  }
+}
+const jsonPerson = JSON.stringify(person)
+const jsPerson = JSON.parse(jsonPerson)
+*/
+
+/* DATES
+// Générer une date
+const date = new Date(year, month, day, hours, min, sec, milli)
+const currentDate = new Date()
+const specificDate(2024, 2, 24, 18)
+
+// Données de date
+const year = currentDate.getFullYear()
+const mouth = currentDate.getMonth
+console.log(`Year :" ${year}`)
+
+// Formats de date :
+date.toDateString()
+date.toISOString()
+date.toLocalString()
+
+// Utilisation de date
+date.setDate(date.getDate() + 1)
+*/
+
+/* SETS (auto executions)
+setInterval(function() {})
+const intervalFunc = setInterval(() => console.log("this function will be executed every 2 secondes"), 2000)
+
+setTimeout(function () {
+    console.log("this function will be executed after 3s")
+}, 3000)
+
+setTimeout(function () {
+    clearinterval(intervalFunc);
+    console.log("interval stopped")
+})
+*/
+
+/* TEMPLATE STRINGS
+Features that allow to create strings with embeded expressions.
+They are denoted by backtics `` instead of single or double quotes.
+It provides a more flexible way to construct strings
+
+console.log(`${function}`)
+Allows to use dynamic strings : console.log(`My name is ${person.age}`)
+*/
+
+/* ARROW FUNCTIONS
+Allows to write sotter functions
+
+function name(para) {
+    console.log(`hello ${para}`)
+}
+Is the same as : 
+name = (para) => {
+    console.log(`hello ${para}`)
+}
+If only one parameter and one line : 
+name = para => console.log(`hello ${para}`)
+
+To store a arrow function into an expression :
+const name = para => console.log(`hello ${para}`)
+*/
+
+/* ENHANCE OBJECT LITTERALS (ES6)
+function user (name, age) {
+    return {
+        name: name,
+        age, age,
+        intro: function () {
+            console.log(`My name is ${name}`)
+        }
+    }
+}
+Is the same as :
+function user (name, age) {
+    return {
+        name,
+        age,
+        intro: () => console.log(`My name is ${name}`),
+    
+    }
+}
+*/
+
+/* DEFAULT FUNCTION PARAMETERS
+When arguments are not provided when calling a function, the default ones are used
+function (para = 2, boll = true)
+*/
+
+/* SPREAD OPERATORS (...)
+Used to make copies of js objects by taking iterable (array, ...) and expanding it to individual elements
+const arr = [1, 2, 3]
+function name(a, b, c) {
+    console.log(a, b, c)
+}
+name(...arr)
+
+let arr1 = [1, 2, 3]
+let arr2 = [4, 5, 6]
+let arr = [...arr1, ...arr2]
+let arr1Copy = [...arr1]
+
+const obj1 = {x: 1, y: 2}
+const obj2 = {z: 3}
+const obj = {...obj1, ...obj2, w: 4}
+*/
+
+/* REST OPERATOR
+allow a function to take an infinite amount of parameters.
+It must be written at the end of all the other parameters
+function name(para1, para2, ...para) {
+    console(para)
+}
+*/
+
+/* DESTRUCTURING
+Allows to unpack values from arrays or objects into distinct variable
+const arr = [1, 2, 3, 4]
+const [a, b] = arr => a=1, b=2;
+const [a, b, , d=0, e=0] = arr => a=1, b=2, d=4, e=0;
+*/
